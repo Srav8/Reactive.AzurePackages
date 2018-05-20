daec182f6db5fe981c004b96b89662ca8ebcd524
SravPV <technosarv@gmail.com>
2018-05-19 20:58:56 -0700

    standardInitialStage

---
+++ Reactive.AzureStorageAdapters/Reactive.AzureStorage.Table/Communicator.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace Reactive.AzureStorage.Table
+{
+    public class Communicator
+    {
+        private readonly string _storageAccountName;
+        private readonly string _storageAccountKey;
+
+        private readonly Lazy<CloudTableClient> _tableClient;
+
+        public Communicator(string storageAccountName, string storageAccountKey)
+        {
+            _storageAccountName = string.IsNullOrEmpty(storageAccountName) ?
+                                    throw new ArgumentNullException(nameof(storageAccountName)) :
+                                    storageAccountName;
+
+            _storageAccountKey = string.IsNullOrEmpty(storageAccountKey) ?
+                throw new ArgumentNullException(nameof(storageAccountKey)) :
+                storageAccountKey;
+
+            var storageCredentials = new StorageCredentials(_storageAccountName, _storageAccountKey);
+
+            _tableClient = new Lazy<CloudTableClient>(
+                () => new CloudStorageAccount(storageCredentials, true).CreateCloudTableClient(),true);
+        }
+
+        public IObservable<T> Read<T>(string tableName, string partitionKey, string rowKey) where T: ITableEntity =>
+            GetCloudTableAsync(tableName)
+                        .Select(tble => (tble, TableOperation.Retrieve<DynamicTableEntity>(partitionKey, rowKey)))
+                        .SelectMany(tple => tple.tble.ExecuteAsync(tple.Item2))
+                        .Select(result => (T)result.Result);
+
+        private IObservable<CloudTable> GetCloudTableAsync(string tableName) =>
+            Observable
+                    .Return(_tableClient.Value.GetTableReference(tableName))
+                    .SelectMany(async tbRef => (tbRef, await tbRef.CreateIfNotExistsAsync()))
+                    .Select(tple => tple.tbRef);
+
+    }
+}
---
+++ Reactive.AzureStorageAdapters/Reactive.AzureStorage.Table/ICommunicator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reactive.AzureStorage.Table
+{
+    public interface ICommunicator
+    {
+    }
+}
